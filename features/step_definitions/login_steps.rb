b5325cb1347e5097ae6da7f98dafa28b06a9824a
Rajdeep <mail.rajvarma@gmail.com>
2015-10-16 16:48:47 +0530

    adding visual validation

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -5,11 +5,12 @@ end
 When(/^I enter invalid credentials$/) do
   query("* id:'username'", :setText=>"Rajdeep")
   query("* id:'password'", :setText=>"Varma")
-
+  nakal_execute('login_screen')
 end
 
 Then(/^I should see error message$/) do
   expect(query("* id:'error'",:text).first).to eq 'Please enter a valid username'
+  nakal_execute('login_screen_with_error')
 end
 
 And(/^I log\-in$/) do
